f1e500d7b55f958a60852bf766028972c2e5cbcc
Tratik Dixit <tratikdixit@Tratiks-MacBook-Air.local>
2021-06-25 12:43:06 +0800

    updates

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
       <Router>
         <Navbar />
         <Switch>
-          <Route path='/' exact component={Home} />
+          <Route path='/portfolio' exact component={Home} />
           <Route path='/project' component={Project} />
           <Route path='/services' component={Services} />
           <Route path='/sign-up' component={SignUp} />
